5aafe9d94e9f1d2204d4ff32d247b82733c7d7d1
LV Soft Communications <lasvarillas@gmail.com>
2015-06-18 20:47:00 -0300

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,2 @@
-{"version":"3.0.6.19","code":"0",downloadURL:"http://lv-soft.github.io/XiTools/xitools.user.js",updateURL:"http://lv-soft.github.io/XiTools/changelog.html"}
+var XiTversion = {"version":"3.0.6.19","code":"0",downloadURL:"http://lv-soft.github.io/XiTools/xitools.user.js",updateURL:"http://lv-soft.github.io/XiTools/changelog.html"};
+XiT.checkVersion(XiTversion);
